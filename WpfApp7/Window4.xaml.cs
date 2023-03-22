7da934c6c7b58a369a2457ff4aad49f29f27668b
Acer <Acer@DESKTOP-GG5PIOL>
2023-03-22 14:15:11 +0300

    Добавьте файлы проекта.

---
+++ WpfApp7/Window4.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp7.Models;
+
+namespace WpfApp7
+{
+    public partial class Window4 : Window
+    {
+        public Window4()
+        {
+            InitializeComponent();
+            AppConnect.database = new keramikaEntities();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window5 window5 = new Window5();
+            window5.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window6 window6 = new Window6();
+            window6.Show();
+        }
+
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                try
+                {
+                    ProductsListBox.ItemsSource = AppConnect.database.ProductsT1.ToList();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Error Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp7/Window7client.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Логика взаимодействия для Window7client.xaml
+    /// </summary>
+    public partial class Window7client : Window
+    {
+        public Window7client()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window8client window8Client = new Window8client();
+            window8Client.Show();
+        }
+
+        private void Registr_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
